b7a3cb0343d40404222a0906f9b63081b2fad8de
Oke Amuwha <gv4281@wayne.edu>
2021-02-21 12:19:21 -0500

    Initial Commit

---
+++ Registration Project/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Registration Project
+//
+//  Created by Oke-Oghene Amuwha on 2/21/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
